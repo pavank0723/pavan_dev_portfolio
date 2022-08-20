2509fc6a7950726dd60e918e72ef9bdd7699c813
pavank0723 <pavan.yadav0723@gmail.com>
2022-08-20 22:14:27 +0530

    creat all components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
-
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Pricing from './components/pricing/Pricing';
+import Portfolio from './components/portfolio/Portfolio';
+import Resume from './components/resume/Resume';
+import Blog from './components/blog/Blog';
+import Contact from './components/contact/Contact';
+import Testimonials from './components/testimonials/Testimonials';
 function App() {
   return <>
-    <h1>Pavan Portfolio</h1>
+  <Sidebar/>
+    <main className='main'>
+    <Home />
+    <About />
+    <Services />
+    <Pricing />
+    <Portfolio />
+    <Resume />
+    <Blog />
+    <Contact />
+    <Testimonials />
+    </main>
   </>
 }
 
