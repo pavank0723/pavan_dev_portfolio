ff9e751c60bcfd3ac479c2f1a33c8da75210c4d1
pavank0723 <pavan.yadav0723@gmail.com>
2022-08-22 23:21:20 +0530

    home component done

---
+++ src/components/home/HeaderSocial.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const HeaderSocial = () => {
+    return (
+        <div className='home_socials'>
+            <a href="" className='home_social_link' target="_blank">
+                <i class="fa-brands fa-square-instagram"></i>
+            </a>
+
+            <a href="" className='home_social_link' target="_blank">
+                <i className='fa-brands fa-square-twitter'></i>
+            </a>
+
+            <a href="" className='home_social_link' target="_blank">
+                <i className='fa-brands fa-square-github'></i>
+            </a>
+
+            <a href="" className='home_social_link' target="_blank">
+                <i className='fa-brands fa-linkedin'></i>
+            </a>
+
+            <a href="" className='home_social_link' target="_blank">
+                <i className='fa-brands fa-square-github'></i>
+            </a>
+
+            <a href="" className='home_social_link' target="_blank">
+                <i className='fa-brands fa-linkedin'></i>
+            </a>
+
+        </div>
+    )
+}
+
+export default HeaderSocial
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./home.css"
+import PavanImg from "../../assets/py.png"
+import HeaderSocial from './HeaderSocial'
+import ScrollDown from './ScrollDown'
+import Shape from './Shape'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home container' id='home'>
+      <div className='intro'>
+
+        <img src={PavanImg} alt="Dev Image" className="home_img"/>
+        <h1 className='home_name'>Pavan Yadav</h1>
+        <span className='home_self_intro'>I'm a Front-End Developer</span>
+
+        <HeaderSocial />
+
+        <a href='#contact' className='btn'> Hire Me</a>
+        <ScrollDown />
+      </div>
+      <Shape />
+    </section>
   )
 }
 
