3978fa6d85dc225b4f161e922b3df6143f0a467b
pavank0723 <pavan.yadav0723@gmail.com>
2022-08-23 18:20:03 +0530

    add type writter dependency

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,15 +4,30 @@ import PavanImg from "../../assets/py.png"
 import HeaderSocial from './HeaderSocial'
 import ScrollDown from './ScrollDown'
 import Shape from './Shape'
+import { useTypewriter  } from 'react-simple-typewriter'
+import 'react-simple-typewriter/dist/index'
 
 const Home = () => {
+  const {text} = useTypewriter({
+    words: [ " Developer ✌."," UI Designer ✨."," Self Learner🌞."," Self Believer✍.",],
+    loop: 0, 
+    typeSpeed: 70,
+    deleteSpeed: 50,
+    delaySpeed: 1000
+    })
   return (
     <section className='home container' id='home'>
       <div className='intro'>
 
-        <img src={PavanImg} alt="Dev Image" className="home_img"/>
+        <img src={PavanImg} alt="Dev Image" className="home_img" />
         <h1 className='home_name'>Pavan Yadav</h1>
-        <span className='home_self_intro'>I'm a Front-End Developer</span>
+        <p className='home_self_intro'>I'm a
+          <span className='home_type_Writter'>
+            
+            {text}
+            <span className="cursor">|</span>
+          </span>
+        </p>
 
         <HeaderSocial />
 
