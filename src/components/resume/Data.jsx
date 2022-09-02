cf502f789a36338d62daf74153e860840a29e668
pavank0723 <pavan.yadav0723@gmail.com>
2022-09-02 18:28:27 +0530

    resume component done

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const Data = [
+    {
+      id: 1,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2021 - present",
+      title: "M.Sc. Information Technology Degree",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+    },
+    {
+      id: 2,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2017 - 2020",
+      title: "B.Sc. Information Technology Degree",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+    },
+    {
+      id: 3,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2016 - 2017",
+      title: "H.S.C.",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+    },
+    {
+      id: 4,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2019 - present",
+      title: "Web Designer",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+    },
+    {
+      id: 5,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2013 - 2017",
+      title: "Front-End Developer",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+    },
+    {
+      id: 6,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2009 - 2013",
+      title: "Back-End Developer",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+    },
+    {
+        id: 7,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2014 - 2015",
+        title: "S.S.C.",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+      },
+      {
+        id: 3,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2016 - 2017",
+        title: "Flutter Developement",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerciad nec admodum perfecto.",
+      },
+  ];
+
+export default Data
